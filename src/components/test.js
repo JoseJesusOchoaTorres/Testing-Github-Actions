e7ddcd7e42bfca7573c4275593ae459621dc2dec
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2020-10-29 01:04:50 -0600

    Commenting constant.

--- src/components/test.js
+++ src/components/test.js
@@ -1,25 +1,34 @@
+/** @constant
+    @type {string}
+    @default
+*/
+const helloMessage = `Hello there `;
 
-    const helloMessage = `Hello there `
+/** @constant
+    @type {string}
+    @default
+*/
+const goodbyeMessage = 'Goodbye ';
 
-    /**
-     * This is a simple JSDoc example; this function will return
-     * a hello message with a full name.
-     *
-     * @param {string} name - The name
-     * @param {string} lastName - The Last name
-     * @returns {string}
-     */
-    const helloName = (name, lastName) =>`${helloMessage} ${name} ${lastName}`
+/**
+ * This is a simple JSDoc example; this function will return
+ * a hello message with a full name.
+ *
+ * @param {string} name - The name
+ * @param {string} lastName - The Last name
+ * @returns {string}
+ */
+const helloName = (name, lastName) => `${helloMessage} ${name} ${lastName}`;
 
-    /**
-     * This is a simple JSDoc example; this function will return
-     * a goodbye message with a full name.
-     *
-     * @param {string} name - The name
-     * @param {string} lastName - The Last name
-     * @returns {string}
-     */
-    const goodbyeName = (name, lastName) =>`${helloMessage} ${name} ${lastName}`
+/**
+ * This is a simple JSDoc example; this function will return
+ * a goodbye message with a full name.
+ *
+ * @param {string} name - The name
+ * @param {string} lastName - The Last name
+ * @returns {string}
+ */
+const goodbyeName = (name, lastName) => `${goodbyeMessage} ${name} ${lastName}`;
 
-    helloName('Jose Jesus', 'Ochoa Torres')
-    goodbyeName('Jose Jesus', 'Ochoa Torres')
+helloName('Jose Jesus', 'Ochoa Torres');
+goodbyeName('Jose Jesus', 'Ochoa Torres');
