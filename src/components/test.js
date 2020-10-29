143fbf1c4d262bd94fe8e0e3d69187bc4d763310
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2020-10-29 00:33:40 -0600

    Moving API-README to Documentation.

--- src/components/test.js
+++ src/components/test.js
@@ -11,4 +11,15 @@
      */
     const helloName = (name, lastName) =>`${helloMessage} ${name} ${lastName}`
 
+    /**
+     * This is a simple JSDoc example; this function will return
+     * a goodbye message with a full name.
+     *
+     * @param {string} name - The name
+     * @param {string} lastName - The Last name
+     * @returns {string}
+     */
+    const goodbyeName = (name, lastName) =>`${helloMessage} ${name} ${lastName}`
+
     helloName('Jose Jesus', 'Ochoa Torres')
+    goodbyeName('Jose Jesus', 'Ochoa Torres')
