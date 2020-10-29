12df28de8dcb29dc7b9782a81c82e904db0286df
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2020-10-28 23:38:16 -0600

    From JSDoc to Markdown.

---
+++ src/components/test.js
@@ -0,0 +1,14 @@
+
+    const helloMessage = `Hello there `
+
+    /**
+     * This is a simple JSDoc example; this function will return
+     * a hello message with a full name.
+     *
+     * @param {string} name - The name
+     * @param {string} lastName - The Last name
+     * @returns {string}
+     */
+    const helloName = (name, lastName) =>`${helloMessage} ${name} ${lastName}`
+
+    helloName('Jose Jesus', 'Ochoa Torres')
